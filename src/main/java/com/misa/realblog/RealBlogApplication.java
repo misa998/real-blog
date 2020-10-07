cd0587bc48608fe3508f7b4c2b77c343a9b70015
misa998 <milosilic998@gmail.com>
2020-10-07 12:32:41 +0200

    first commit

---
+++ src/main/java/com/misa/realblog/RealBlogApplication.java
@@ -0,0 +1,13 @@
+package com.misa.realblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misa/realblog/RealBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misa.realblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
